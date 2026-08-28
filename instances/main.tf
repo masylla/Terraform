96d48cd3945589c9bcb8f33f94e0edfa072b2fb3
masylla <masylla@hotmail.com>
2026-08-28 15:31:10 -0400

    Update VPC configuration

---
+++ instances/main.tf
@@ -0,0 +1,69 @@
+resource "aws_instance" "lesson_05" {
+  ami           = "ami-0bdc7d025135d7b49"
+  instance_type = "t3.micro"
+  key_name      = "aws_key"
+  vpc_security_group_ids = [
+    aws_security_group.sg_ssh.id,
+    aws_security_group.sg_https.id,
+    aws_security_group.sg_http.id
+  ]
+
+  tags = {
+    Name                    = "Lesson_05"
+    Vanquisher_of_Gothmog   = "Ecthelion"
+    Vanquisher_of_Ecthelion = "Gothmog"
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBX4/VjapiTnKz7aFw8Cw35onYJuaU0l/5qp4O4VmhKx mamadousylla@Mac"
+}
+
+resource "aws_security_group" "sg_ssh" {
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+}
+
+resource "aws_security_group" "sg_https" {
+  ingress {
+    cidr_blocks = ["192.168.0.0/16"]
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+}
+
+resource "aws_security_group" "sg_http" {
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+}
